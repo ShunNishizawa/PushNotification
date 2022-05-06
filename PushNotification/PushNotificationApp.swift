6a2dc1488618480fdff499f53af93bf47ea53a45
NishizawaShun <nishizawa.shun@appsoftlab.com>
2022-05-07 07:17:22 +0900

    FirestCommit

---
+++ PushNotification/PushNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationApp.swift
+//  PushNotification
+//
+//  Created by 西澤駿 on 2022/05/07.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
